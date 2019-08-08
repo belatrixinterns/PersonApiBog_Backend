292d3c673436d966828d6cce34486783bcab4845
andmunozmm <andmunoz@belatrixsf.com>
2019-08-08 10:59:06 -0500

    DefaultException created

---
+++ src/main/java/com/person/api/exception/DefaultException.java
@@ -0,0 +1,11 @@
+package com.person.api.exception;
+
+public class DefaultException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public DefaultException(String message) {
+		super(message);
+	}
+
+}
