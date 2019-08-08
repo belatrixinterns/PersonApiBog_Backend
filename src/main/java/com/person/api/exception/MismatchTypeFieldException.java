558d4332f31b297ecd13fab4290c310f22a8a92b
andmunozmm <andmunoz@belatrixsf.com>
2019-08-08 10:42:54 -0500

    MismatchTypeFieldException created

---
+++ src/main/java/com/person/api/exception/MismatchTypeFieldException.java
@@ -0,0 +1,10 @@
+package com.person.api.exception;
+
+public class MismatchTypeFieldException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public MismatchTypeFieldException(String message) {
+		super(message);
+	}
+}
