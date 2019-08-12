c549013117b25490fb299dda63cf82aadf38a48d
aariza-Belatrixsf <aariza@belatrixsf.com>
2019-08-12 16:32:22 -0500

    Now attribute 'contact' is added to person table. Fixed mistake in converter. This update, uses Andres' DB, not server DB

--- src/main/java/com/person/api/converter/PersonConverter.java
+++ src/main/java/com/person/api/converter/PersonConverter.java
@@ -9,12 +9,12 @@ import com.person.api.entity.PersonEntity;
 public class PersonConverter {
 
     public static PersonDto convertToDto(PersonEntity usuarioEntity){
-        return new PersonDto(usuarioEntity.getDocument_type(), usuarioEntity.getName(), usuarioEntity.getLast_name(),
+        return new PersonDto(usuarioEntity.getDocument_id(), usuarioEntity.getName(), usuarioEntity.getLast_name(),
          usuarioEntity.getDate_of_birth(), usuarioEntity.getDocument_type(), usuarioEntity.getGender(), usuarioEntity.getNationality(), usuarioEntity.getContact());
     }
 
     public static PersonEntity convertToEntity(PersonDto usuarioDto){
-        return new PersonEntity(null, usuarioDto.getDocument_type(), usuarioDto.getName(), usuarioDto.getLast_name(),
+        return new PersonEntity(null, usuarioDto.getDocument_id(), usuarioDto.getName(), usuarioDto.getLast_name(),
          usuarioDto.getDate_of_birth(), usuarioDto.getDocument_type(), usuarioDto.getGender(), usuarioDto.getNationality(), usuarioDto.getContact());
     }
 }
